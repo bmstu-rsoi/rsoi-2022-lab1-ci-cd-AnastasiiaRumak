52be465904b73484440733823df453dd0a9f6f6a
stasia.beliver <ms.rumak@gmail.com>
2022-11-24 11:43:36 +0300

    ??

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-	_ "database/sql"
+	_"database/sql"
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
 	_ "github.com/lib/pq" // ...
@@ -28,7 +28,10 @@ const (
 
 func main() {
 	//db, err := sqlx.Connect("postgres", fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", "localhost",5432,"postgres", "postgres", "postgres"))
+	
 	db, err := sqlx.Connect("postgres", os.Getenv("DATABASE_URL"))
+	fmt.Println("PATH:", db)
+	//db, err := sqlx.Connect("postgres", fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", "ec2-52-86-56-90.compute-1.amazonaws.com",5432,"qbhewqmzoqeisd", "9a72432985934116cb32a55215dec1344005a2902da1539a1e2e261a30bf7486", "da2uvheuu1e2r1"))
 	if err != nil {
 		log.Fatal(fmt.Errorf("error connecting to database: %w", err))
 	}
@@ -42,4 +45,5 @@ func main() {
 
 	//log.Fatal(e.Start("localhost:8089"))
 	log.Fatal(e.Start(fmt.Sprintf(":%s", os.Getenv("PORT"))))
+	//log.Fatal(e.Start("localhost:8089"))
 }
